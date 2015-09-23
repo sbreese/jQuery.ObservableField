d3423f1989dd7f56d66c55e7c418dea49ce0a1d3
sbreese <sbreese@gmail.com>
2015-09-23 10:17:45 -0500

    Create jquery.observe_field_real_time.js

---
+++ jquery.observe_field_real_time.js
@@ -0,0 +1,47 @@
+// jquery.observe_field_real_time.js
+
+(function( $ ){
+
+  jQuery.fn.observe_field_real_time = function(frequency, callback) {
+
+    return this.each(function(){
+      var $this = $(this);
+      var prev = $this.val();
+      var prevChecked = $this.prop('checked');
+
+      var check = function() {
+        if(removed()){ // if removed clear the interval and don't fire the callback
+          if(ti) clearInterval(ti);
+          return;
+        }
+
+        var val = $this.val();
+        var checked = $this.prop('checked');
+        if(prev != val || checked != prevChecked ){
+          prev = val;
+          prevChecked = checked;
+          $this.map(callback); // invokes the callback on $this
+        }
+      };
+
+      var removed = function() {
+        return $this.closest('html').length == 0
+      };
+
+      var reset = function() {
+        if(ti){
+          clearInterval(ti);
+          ti = setInterval(check, frequency);
+        }
+      };
+
+      check();
+      var ti = setInterval(check, frequency); // invoke check periodically
+
+      // reset counter after user interaction
+      $this.bind('keyup click mousemove', reset); //mousemove is for selects
+    });
+
+  };
+
+})( jQuery );
